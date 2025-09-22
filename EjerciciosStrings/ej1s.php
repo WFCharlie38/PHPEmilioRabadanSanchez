758c6e57b588d085d656cc78a5dfa129920d2eee
Emilio Rabadan Sanchez <wfcharlie38@gmail.com>
2025-09-22 10:12:41 +0200

    ej1s.php correcto

--- EjerciciosStrings/ej1s.php
+++ EjerciciosStrings/ej1s.php
@@ -4,8 +4,22 @@
 <?php
 $ip="192.18.16.204";
 
-$byte1 = (int)substr($ip,0,3);
-echo $byte1;
+$p1 = strpos($ip, ".");
+$p2 = strpos($ip, ".", $p1 + 1);
+$p3 = strpos($ip, ".", $p2 + 1);  
+
+$byte1 = (int)substr($ip, 0, $p1);
+$byte2 = (int)substr($ip, $p1 + 1, $p2 - $p1 - 1);
+$byte3 = (int)substr($ip, $p2 + 1, $p3 - $p2 - 1);
+$byte4 = (int)substr($ip, $p3 + 1);
+
+$bin1 = sprintf("%b", $byte1);
+$bin2 = sprintf("%b", $byte2);
+$bin3 = sprintf("%b", $byte3);
+$bin4 = sprintf("%b", $byte4);
+
+echo "IP $ip en binario es $bin1.$bin2.$bin3.$bin4"
+
 ?>
 </BODY>
 </HTML>
