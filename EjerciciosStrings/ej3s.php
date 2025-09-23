148f58c393251555edf58cb585f49095790688c0
Emilio Rabadan Sanchez <wfcharlie38@gmail.com>
2025-09-23 10:40:52 +0200

    ejs3.php terminado

--- EjerciciosStrings/ej3s.php
+++ EjerciciosStrings/ej3s.php
@@ -15,27 +15,26 @@ $byte2 = (int)substr($ip, $p1 + 1, $p2 - $p1 - 1);
 $byte3 = (int)substr($ip, $p2 + 1, $p3 - $p2 - 1);
 $byte4 = (int)substr($ip, $p3 + 1);
 
-$binario = sprintf('%08bd.%08b.%08bd.%08b', $byte1, $byte2, $byte2, $byte4);
-$binarioTrans = (substr($binario,0,32-$mascara+1));
-$binarioRed = str_pad($binarioTrans, 32, "1", STR_PAD_RIGHT);
-$binarioBroadcast = str_pad($binarioTrans, 32, "0", STR_PAD_RIGHT);
+$binario = sprintf('%08b%08b%08b%08b', $byte1, $byte2, $byte3, $byte4);
+$binarioTrans = (substr($binario,0,32-$mascara));
+$binarioRed = str_pad($binarioTrans, 32, "0", STR_PAD_RIGHT);
+$binarioBroadcast = str_pad($binarioTrans, 32, "1", STR_PAD_RIGHT);
 
-$red1 = (int)substr($binarioRed, 0, 7);
-$red2 = (int)substr($binarioRed, 8, 15);
-$red3 = (int)substr($binarioRed, 16, 23);
-$red4 = (int)substr($binarioRed, 24, 31);
+$red1 = (int)substr($binarioRed, 0, 8);
+$red2 = (int)substr($binarioRed, 8, 8);
+$red3 = (int)substr($binarioRed, 16, 8);
+$red4 = (int)substr($binarioRed, 24, 8);
 
-$direccionRed = sprintf('%d.%d.%d.%d', $red1, $red2, $red3, $red4);
+$direccionRed = sprintf('%d.%d.%d.%d',bindec($red1),bindec($red2),bindec($red3),bindec($red4));
+$rango1 = sprintf('%d.%d.%d.%d',bindec($red1),bindec($red2),bindec($red3),bindec($red4)+1);
 
-$red1 = (int)substr($binarioRed, 0, 7);
-$red2 = (int)substr($binarioRed, 8, 15);
-$red3 = (int)substr($binarioRed, 16, 23);
-$red4 = (int)substr($binarioRed, 24, 31);
+$broad1 = (int)substr($binarioBroadcast, 0, 8);
+$broad2 = (int)substr($binarioBroadcast, 8, 8);
+$broad3 = (int)substr($binarioBroadcast, 16, 8);
+$broad4 = (int)substr($binarioBroadcast, 24, 8);
 
-$direccionBroadcast = ;
-
-$rango1 = ;
-$rango2 = ;
+$direccionBroadcast = sprintf('%d.%d.%d.%d', bindec($broad1),bindec($broad2),bindec($broad3),bindec($broad4));
+$rango2 = sprintf('%d.%d.%d.%d',bindec($broad1),bindec($broad2),bindec($broad3),bindec($broad4)-1);
 
 echo "IP $ip </br>";
 echo "MASCARA $mascara </br>";
